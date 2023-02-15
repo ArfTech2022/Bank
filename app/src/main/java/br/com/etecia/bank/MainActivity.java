cc3b5277f1ad95837c85b30844e6e26f1c8e235d
ArfTech2022 <armando.rodrigues@etec.sp.gov.br>
2023-02-14 21:52:11 -0300

    atualização2.0

--- app/src/main/java/br/com/etecia/bank/MainActivity.java
+++ app/src/main/java/br/com/etecia/bank/MainActivity.java
@@ -6,27 +6,43 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ListView idListaValores;
+
+    String titulo[] = {"Lista de Agências","Itaú - Ag. 1608","Itaú - Ag. 0360", "Itaú - Ag. 6414", "Itaú - Ag. 0767"};
+
+    String descricao[] = {"Consulta a Lista de Agências próximas a você do banco Itaú","Chácara Santo Antônio","Av. Santa Catarina", "Itaú Personnalité Marajoara", "Jd. Prudência\n"};
+
+    String nota[] = {"5.0" , "3,4", "3.0" , "5.0", "4,5"};
+
+    int bank[] = {R.drawable.itau,R.drawable.itausantoantonio,R.drawable.santacatarina,R.drawable.marajoarapersonalite,R.drawable.jdmarajoara};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        idListaValores=findViewById(R.id.idListaValores);
-        MyAdapter adapter=new MyAdapter();
+
+        idListaValores = findViewById(R.id.idListaValores);
+
+        MyAdapter adapter = new MyAdapter();
+
         idListaValores.setAdapter(adapter);
 
 
     }
+
     public class MyAdapter extends BaseAdapter {
 
 
         @Override
         public int getCount() {
-            return 0;
+
+            return bank.length;
         }
 
         @Override
@@ -41,7 +57,26 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
+
+
+            ImageView idmdlimg;
+            TextView idtxtitulo, idMdlDescricao, idmdlnota;
+
+
+            View v = getLayoutInflater().inflate(R.layout.modelo_banco, null);
+
+
+            idmdlimg = v.findViewById(R.id.idmdlimg);
+            idtxtitulo = v.findViewById(R.id.idtxtitulo);
+            idMdlDescricao = v.findViewById(R.id.idMdlDescricao);
+            idmdlnota = v.findViewById(R.id.idmdlnota);
+
+            idtxtitulo.setText(titulo[i]);
+            idMdlDescricao.setText(descricao[i]);
+            idmdlnota.setText(nota[i]);
+            idmdlimg.setImageResource(bank[i]);
+
+            return v;
         }
     }
 }
